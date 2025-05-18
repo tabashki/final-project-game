a74e7159a5b06d25b62f697978b387879e840d84
QubitShifter <physicsenthusiast@yahoo.com>
2025-05-18 13:11:48 +0300

    SpriteSheets functionality

---
+++ Game/FrameData.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using Microsoft.Xna.Framework;
+
+namespace TeamCherry.Project;
+
+    public class FrameData
+    {
+        public Frame frame { get; set; }
+    }
+
+    public class Frame
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        public int w { get; set; }
+        public int h { get; set; }
+    }
+
+    public class AsepriteSheet
+    {
+        public List<FrameData> frames { get; set; }
+
+        public static Rectangle[] LoadSourceRects(string json)
+        {
+            var sheet = JsonConvert.DeserializeObject<AsepriteSheet>(json);
+            return sheet.frames.Select(f => new Rectangle(f.frame.x, f.frame.y, f.frame.w, f.frame.h)).ToArray();
+        }
+    }
+
+
--- Game/Player.cs
+++ Game/Player.cs
@@ -13,7 +13,18 @@ class Player : Entity
     private Vector2 targetVelocity = Vector2.Zero;
     private Vector2 moveInput = Vector2.Zero;
 
-    public Player(Texture2D texture) : base(texture) { }
+    private readonly SpriteSheet spriteSheet;
+    public int SpriteIndex { get; set; } = 0;
+
+    public override SpriteEffects SpriteEffects => (flipHoriz) ? SpriteEffects.FlipHorizontally : SpriteEffects.None;
+
+    // Override the SourceRectangle to use the current frame from the sprite sheet
+    public override Rectangle? SourceRectangle => spriteSheet?.GetFrame(SpriteIndex);
+
+    public Player(Texture2D texture, SpriteSheet sheet) : base(texture)
+    {
+        spriteSheet = sheet;
+    }
 
     public void SetMoveInput(Vector2 input)
     {
@@ -49,7 +60,15 @@ class Player : Entity
             SpriteEffects = SpriteEffects.None;
         }
 
-        Vector2 center = Position + new Vector2(Texture.Width / 2f, Texture.Height / 2f);
+        if (spriteSheet.FrameCount > 0)
+        {
+            SpriteIndex = (SpriteIndex + 1) % spriteSheet.FrameCount;
+        }
+
+        // Example: switch animation frame every update (you can replace this with animation logic)
+        SpriteIndex = (SpriteIndex + 1) % spriteSheet.FrameCount;
+
+        Vector2 center = Position + new Vector2((SourceRectangle?.Width ?? Texture.Width) / 2f, (SourceRectangle?.Height ?? Texture.Height) / 2f);
         DebugDraw.Arrow(center, center + moveInput * 20, 3, Color.Blue);
         DebugDraw.Arrow(center, center + Velocity * 0.2f, 3, Color.White);
         DebugDraw.Rect(BoundingBox, Color.Yellow);
