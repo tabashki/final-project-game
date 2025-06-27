531a974cc55523648247384964d3ceb48e7a89e2
evad321 <evadimitrova119@gmail.com>
2025-06-27 18:07:40 +0300

    level-system

---
+++ Game/Levels.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+
+namespace TeamCherry.Project;
+
+public static class Levels
+{
+    public static Level CreateLevel1(Texture2D playerTexture)
+    {
+        var level = new Level();
+        Player player = new Player(playerTexture);
+        player.SetPosition(new Vector2(100, 100));
+        level.AddEntity(player);
+        return level;
+    }
+
+    public static Level CreateLevel2(Texture2D playerTexture)
+    {
+        var level = new Level();
+        Player player = new Player(playerTexture);
+        player.SetPosition(new Vector2(80, 75));
+        level.AddEntity(player);
+        return level;
+    }
+    public static Level CreateLevel3(Texture2D playerTexture)
+    {
+        var level = new Level();
+        Player player = new Player(playerTexture);
+        player.SetPosition(new Vector2(40, 65));
+        level.AddEntity(player);
+        return level;
+    }
+
+}
+
