9bfd69f4c66432f936c4cfd575072b430b168d11
CloseTheDie <37995094+CloseTheDie@users.noreply.github.com>
2025-06-29 13:39:35 +0300

    Changing to "init" instead of "set" for JSON properties

--- Game/Graphics/Map/Tile.cs
+++ Game/Graphics/Map/Tile.cs
@@ -5,8 +5,8 @@ namespace TeamCherry.Project;
 public class Tile
 {
     [JsonPropertyName("id")]
-    public int Id { get; set; }
+    public int Id { get; init; }
 
     [JsonPropertyName("properties")]
-    public List<TileProperty>? Properties { get; set; }
+    public List<TileProperty>? Properties { get; init; }
 }
