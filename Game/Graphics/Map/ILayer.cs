47665931c928237c3178ed65625b0464f0181b25
CloseTheDie <37995094+CloseTheDie@users.noreply.github.com>
2025-06-29 13:38:07 +0300

    Changing to "init" instead of "set" for JSON properties

--- Game/Graphics/Map/ILayer.cs
+++ Game/Graphics/Map/ILayer.cs
@@ -2,12 +2,12 @@ namespace TeamCherry.Project;
 
 public interface ILayer
 {
-    public string? Name { get; set; }
-    public int LayerWidth { get; set; }  // Should usually be same as MapWidth
-    public int LayerHeight { get; set; }  // Should usually be same as MapHeight
-    public int[]? Data { get; set; }
-    public string? Type { get; set; }
-    public bool Visible { get; set; }
-    public int Id { get; set; }
+    public string? Name { get; init; }
+    public int LayerWidth { get; init; }  // Should usually be same as MapWidth
+    public int LayerHeight { get; init; }  // Should usually be same as MapHeight
+    public int[]? Data { get; init; }
+    public string? Type { get; init; }
+    public bool Visible { get; init; }
+    public int Id { get; init; }
 
 }
